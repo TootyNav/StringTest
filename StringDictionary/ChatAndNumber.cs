ae84653dd7f1dadddedf26f2f198b86ef5b66d16
Tooty <navid2007@hotmail.com>
2017-09-06 23:22:12 +0100

    Add files via upload

---
+++ StringDictionary/ChatAndNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDictionary
+{
+    class ChatAndNumber
+    {
+        public int no { get; set; }
+        public char ch { get; set; }
+    }
+}
---
+++ StringDictionary/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newWord = "Hello World";
+            //var sd = new Dictionary<int, char>();
+            var cn = new List<ChatAndNumber>();
+            var sequance = 1;
+
+            foreach (var item in newWord)
+            {
+                sequance = 1;
+                foreach (var item2 in cn)
+                {
+                    if (item2.ch == item)
+                    {
+                        item2.no += 1;
+                        sequance = item2.no;
+                        break;
+
+                    }
+
+                }
+
+                cn.Add(new ChatAndNumber() { no = sequance, ch = item });
+
+            }
+
+            for (int i = 0; i < cn.Count; i++)
+            {
+                Console.WriteLine(cn[i].no + " and " + cn[i].ch);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
